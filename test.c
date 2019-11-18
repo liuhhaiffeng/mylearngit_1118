20f32bc3d51044cd456b0cd6bef23d3525040292
liuhhaiffeng <liuhhaiffeng@163.com>
2019-11-18 09:53:36 +0800

    add new file LICENCE

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h> // strlen()
+
+int main()
+{
+    const char *str = "a209e71f1061d79d7fd5402021931bbd6906ff03";
+    printf("str.len = %d\n", strlen(str));
+}
